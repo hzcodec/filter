f1edebc5e6dc5ae59ebf1f1f1232953b340f9700
heinz.samuelsson <heinz.samuelsson@unjo.com>
2017-03-08 17:00:25 +0100

    Mupp

--- main_ring_buffer.c
+++ main_ring_buffer.c
@@ -5,38 +5,33 @@
 
 int main(int argc, char *argv[])
 {
-
-//typedef struct
-//{
-//     int* buffer;
-//     int* buffer_end;
-//     int* data_start;
-//     int* data_end;
-//     int  count;
-//     int  size;
-// } RingBuffer;
-
     RingBuffer myBuff;
     int value = 0;
 
     rb_init(&myBuff, 8);
 
-    rb_push(&myBuff, 100);
-    rb_push(&myBuff, 200);
-    rb_push(&myBuff, 300);
+    for (int i=0; i<10; i++)
+    {
+        rb_push(&myBuff, 100+i);
+    }
 
-    printf("[0]:%d\n", myBuff.buffer[0]);
-    printf("[1]:%d\n", myBuff.buffer[1]);
-    printf("[2]:%d\n", myBuff.buffer[2]);
+    printf("buffer_end:%d\n", *myBuff.buffer_end);
+    printf("data_start:%d\n", *myBuff.data_start);
+    printf("data_end:%d\n", *myBuff.data_end);
+    printf("count:%d\n", myBuff.count);
+    printf("size:%d\n", myBuff.size);
 
-    value = rb_pop(&myBuff);
-    printf("value:%d\n", value);
-    value = rb_pop(&myBuff);
-    printf("value:%d\n", value);
-    value = rb_pop(&myBuff);
-    printf("value:%d\n", value);
-    value = rb_pop(&myBuff);
-    printf("value:%d\n", value);
+    for (int i=0; i<8; i++)
+    {
+        value = rb_pop(&myBuff);
+        printf("value[%d]:%d\n", i, value);
+    }
+
+    printf("buffer_end:%d\n", *myBuff.buffer_end);
+    printf("data_start:%d\n", *myBuff.data_start);
+    printf("data_end:%d\n", *myBuff.data_end);
+    printf("count:%d\n", myBuff.count);
+    printf("size:%d\n", myBuff.size);
 
     return 0;
 }
