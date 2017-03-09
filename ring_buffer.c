76ff8d5f72881ecb2b7a99e78e8b0bdf7b71150d
Heinz <heinz.samuelsson@gmail.com>
2017-03-09 10:02:03 +0100

    Code cleanup

--- ring_buffer.c
+++ ring_buffer.c
@@ -30,7 +30,9 @@ void rb_free(RingBuffer* rb)
 bool rb_push(RingBuffer* rb, int data)
 {
     if (rb == NULL || rb->buffer == NULL)
+    {
         return false;
+    }
 
     *rb->data_end = data;
     //printf("1.%s() - data_end: %d\n", __func__, *rb->data_end);
@@ -39,14 +41,12 @@ bool rb_push(RingBuffer* rb, int data)
     if (rb->data_end == rb->buffer_end)
     {
         rb->data_end = rb->buffer;
-        //printf("2.%s() - data_end:%d, buffer_end:%d\n", __func__, *rb->data_end, *rb->buffer_end);
     }
 
     if (rb_full(rb))
     {
         if ((rb->data_start + 1) == rb->buffer_end)
         {
-            //printf("3.%s() - data_start:%d, buffer_end:%d\n", __func__, *rb->data_start, *rb->buffer_end);
             rb->data_start = rb->buffer;
 	}
         else
@@ -57,7 +57,6 @@ bool rb_push(RingBuffer* rb, int data)
     else
     {
         rb->count++;
-        //printf("4.%s() - count: %d\n", __func__, rb->count);
     }
 
     printf("%s() - start:%p, end: %p, buffer_end:%p, count:%d\n", __func__, rb->data_start, rb->data_end, rb->buffer_end, rb->count);
@@ -67,12 +66,18 @@ bool rb_push(RingBuffer* rb, int data)
 int rb_pop(RingBuffer* rb)
 {
     if (rb == NULL || rb->buffer == NULL)
+    {
         return false;
+    }
 
     int data = *rb->data_start;
     rb->data_start++;
+
     if (rb->data_start == rb->buffer_end)
+    {
         rb->data_start = rb->buffer;
+    }
+
     rb->count--;
 
     printf("%s() - start:%p, end: %p, buffer_end:%p, count:%d\n", __func__, rb->data_start, rb->data_end, rb->buffer_end, rb->count);
@@ -82,6 +87,5 @@ int rb_pop(RingBuffer* rb)
 
 bool rb_full(RingBuffer* rb)
 {
-    //printf("%s() - count: %d, size:%d\n", __func__, rb->count, rb->size);
     return rb->count == rb->size;
 }
