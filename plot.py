4095c6388685b2abc38b744e950c08375af84c21
Heinz <heinz.samuelsson@gmail.com>
2017-03-18 18:06:09 +0100

    plot updated

--- plot.py
+++ plot.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+import os
 import sys
 from pylab import *
 
@@ -13,71 +14,73 @@ font = {'family': 'monospace',
         }
 
 data = []
-skip = 0
+lines1 = []
+num_lines = 0
+select = -1
 
+f = []
+for (dirpath, dirnames, filenames) in os.walk('logfiles'):
+    f.extend(filenames)
 
-try:
-    sel = sys.argv[1]
-except IndexError:
-    print 'Error! You must enter a parameter ./plot [1-6]'
-    print '[1] Sinus samples read'
-    print '[2] Square samples read'
-    print '[3] Ramp samples read'
-    print '[4] Slope samples read'
-    print '[5] Filter read'
-    print '[6] Counter read'
-    print '[7] Power of 2 read'
-    sys.exit()
+print '-'*25
+print 'Current logfiles:'
+for i in range(1,len(filenames)):
+    print ' ', filenames[i]
+print ' '
 
+print '[0] Quit application'
+print '[1] Sinus samples read'
+print '[2] Square samples read'
+print '[3] Ramp samples read'
+print '[4] Slope samples read'
+print '[5] Filter read'
+print '[6] Counter read'
+print '[7] Power of 2 read'
+print ' '
 
-if (sel == '1'):
+select = input("Select file to be read: ")
+print 'select ->', select
+
+if (select == 1):
     print 'Sinus samples read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/sinus_samples.txt')]
     num_lines = sum(1 for line in open('logfiles/sinus_samples.txt'))
 
-elif (sel == '2'):
+elif (select == 2):
     print 'Square samples read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/square_samples.txt')]
     num_lines = sum(1 for line in open('logfiles/square_samples.txt'))
 
-elif (sel == '3'):
+elif (select == 3):
     print 'Ramp samples read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/ramp_samples.txt')]
     num_lines = sum(1 for line in open('logfiles/ramp_samples.txt'))
 
-elif (sel == '4'):
+elif (select == 4):
     print 'Slope samples read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/slope.txt')]
     num_lines = sum(1 for line in open('logfiles/slope.txt'))
 
-elif (sel == '5'):
+elif (select == 5):
     print 'Filter read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/filter.txt')]
     num_lines = sum(1 for line in open('logfiles/filter.txt'))
 
-elif (sel == '6'):
+elif (select == 6):
     print 'Counter read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/counter_samples.txt')]
     num_lines = sum(1 for line in open('logfiles/counter_samples.txt'))
 
-elif (sel == '7'):
+elif (select == 7):
     print 'Power of 2 read\n'
     lines1 = [line.rstrip('\n') for line in open('logfiles/power2_samples.txt')]
     num_lines = sum(1 for line in open('logfiles/power2_samples.txt'))
 
-
-try:
-    t = arange(0, num_lines-NUMBER_OF_FIELDS, 1)
-except NameError:
-    print 'Error! Incorrect choise, only between 1-6'
-    print '[1] Sinus samples read'
-    print '[2] Square samples read'
-    print '[3] Ramp samples read'
-    print '[4] Slope samples read'
-    print '[5] Filter read'
-    print '[6] Counter read'
-    print '[7] Power of 2 read'
+elif (select == 0):
+    print 'Quit application'
     sys.exit()
+
+t = arange(0, num_lines-NUMBER_OF_FIELDS, 1)
 
 # calculate total time and convert to ms
 totTime = (num_lines-NUMBER_OF_FIELDS) * SAMPLING_TIME / 1000.0
